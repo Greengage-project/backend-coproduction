c0c9ecb867ce46f1e1311bddaec01caf60b3c836
fvergaracl <fvergaracl@gmail.com>
2025-05-22 16:11:16 +0200

    Remove await from requests.post in action function

--- coproduction/app/api/api_v1/new_games.py
+++ coproduction/app/api/api_v1/new_games.py
@@ -363,7 +363,7 @@ async def action(
         }
     )
     print('----------------------------END DEBUG------------------------------------')
-    response = await requests.post(
+    response = requests.post(
         f"http://{service_name}users/{ str(user_id)}/actions",
         json={
             "typeAction": "new_contribution",
